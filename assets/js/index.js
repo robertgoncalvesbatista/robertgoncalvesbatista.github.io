53ceb2bc64d821ba3e2f4bc571dc9bc4b8fe2ad8
RobertBatista <robertbatista@e-get.com.br>
2024-12-31 01:43:16 -0300

    refactor: projects section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+/* Theme mode */
 let header = document.querySelector("header");
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
@@ -9,11 +10,11 @@ window.addEventListener("scroll", () => {
 menu.onclick = () => {
   navbar.classList.toggle("active");
 };
+
 window.onscroll = () => {
   navbar.classList.remove("active");
 };
 
-// Dark Mode / light mode
 let darkmode = document.querySelector("#darkmode");
 
 darkmode.onclick = () => {
@@ -25,3 +26,31 @@ darkmode.onclick = () => {
     document.body.classList.remove("active");
   }
 };
+
+/* Infinite scroll images */
+const scrollers = document.querySelectorAll(".scroller");
+
+// If a user hasn't opted in for recuded motion, then we add the animation
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+  addAnimation();
+}
+
+function addAnimation() {
+  scrollers.forEach((scroller) => {
+    // add data-animated="true" to every `.scroller` on the page
+    scroller.setAttribute("data-animated", true);
+
+    // Make an array from the elements within `.scroller-inner`
+    const scrollerInner = scroller.querySelector(".scroller__inner");
+    const scrollerContent = Array.from(scrollerInner.children);
+
+    // For each item in the array, clone it
+    // add aria-hidden to it
+    // add it into the `.scroller-inner`
+    scrollerContent.forEach((item) => {
+      const duplicatedItem = item.cloneNode(true);
+      duplicatedItem.setAttribute("aria-hidden", true);
+      scrollerInner.appendChild(duplicatedItem);
+    });
+  });
+}
